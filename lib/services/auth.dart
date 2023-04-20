bdc9a585e443bdf1f4d170edc0643c6b124a0d71
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2023-04-20 22:26:09 +0530

    New Changes Done

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -3,13 +3,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future signInAnon() async {
+  Future singnAnon() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
-      User user = result.user;
+      FirebaseUser user = result.user;
       return user;
-    } catch (error) {
-      print(error.toString());
+    } catch (e) {
+      print(e.toString());
       return null;
     }
   }
