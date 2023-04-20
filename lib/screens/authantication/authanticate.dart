0a465907f032496f3833c00ebd26458e8572b640
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2023-04-20 16:07:49 +0530

    New Changes Done

---
+++ lib/screens/authantication/authanticate.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Authanticate extends StatefulWidget {
+  const Authanticate({super.key});
+
+  @override
+  State<Authanticate> createState() => _AuthanticateState();
+}
+
+class _AuthanticateState extends State<Authanticate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Authanticate'),
+    );
+  }
+}
